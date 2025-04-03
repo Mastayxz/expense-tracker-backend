5f1076b208a1f34e3625954771384c9e5bdd9148
Mastayxz <gustibagus826@gmail.com>
2025-04-03 22:25:49 +0800

    next commit

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoutes = require("./routes/authRoutes"); // ← pastikan path benar
+const authRoutes = require("./routes/authRoutes");
 const walletRoutes = require("./routes/walletRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
+const connectDB = require("./config/db"); // Mengimpor koneksi DB
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// connect to mongo
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.error("❌ MongoDB Error:", err.message));
+// connect to MongoDB using connectDB
+connectDB(); // Memanggil koneksi database dari config
 
 // use routes
-app.use("/api/auth", authRoutes); // ← pastikan ini ada
+app.use("/api/auth", authRoutes);
 app.use("/api/wallets", walletRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/transactions", transactionRoutes);
